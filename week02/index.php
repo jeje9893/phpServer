ef5386b0ab9caba376c10036ae552d1c32555b11
JG <jeje9893@gmail.com>
2024-09-12 14:55:48 +0900

    이번엔 되라

---
+++ week02/index.php
@@ -0,0 +1,2 @@
+<a href="http://113.198.230.60/week01/poll.html" target=_blank>설문조사 페이지 방문하기</a><br/>
+<a href="http://113.198.230.60/week02/request.html" target=_blank>설문조사 TEST 페이지 방문하기</a>
---
+++ week02/reply.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['username'];
+echo 'username : ' . $name . "<br/>";
+
+$name = $_POST['cars'];
+echo 'cars : ' . $name . "<br/>";
+
+$name = $_POST['vehicle1'];
+echo 'vehicle1 : ' . $name . "<br/>";
+
+$name = $_POST['vehicle2'];
+echo 'vehicle2 : ' . $name . "<br/>";
+
+$name = $_POST['vehicle3'];
+echo 'vehicle3 : ' . $name . "<br/>";
+
+$name = $_POST['fav_language'];
+echo 'fav_language : ' . $name . "<br/>";
+?>
+
+<p>화면을 멋지게 꾸밀 것. 아래에 작성자 학번과 이름을 추가할 것</p>
