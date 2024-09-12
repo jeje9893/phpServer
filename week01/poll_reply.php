23961b572c34b11318f85ce226863742aa240be1
JG <jeje9893@gmail.com>
2024-09-12 14:46:46 +0900

    첫 푸쉬

---
+++ week01/poll_reply.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['sname'];
+echo 'sname : ' . $name . "<br/>";
+
+$num = $_POST['snum'];
+echo 'snum : ' . $num . "<br/>";
+
+$name = $_POST['sdept'];
+echo 'sdept : ' . $name . "<br/>";
+
+$name = $_POST['vehicle2'];
+echo 'vehicle2 : ' . $name . "<br/>";
+
+$name = $_POST['vehicle3'];
+echo 'vehicle3 : ' . $name . "<br/>";
+
+$name = $_POST['fav_language'];
+echo 'fav_language : ' . $name . "<br/>";
+?>
+
+<p>화면을 멋지게 꾸밀 것. 아래에 작성자 학번과 이름을 추가할 것</p>
