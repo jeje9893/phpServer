a9c21e084c621d08dcf1a0983e4b034292dbd791
JG <jeje9893@gmail.com>
2024-09-26 14:45:14 +0900

    알 수 없는 빈칸이 있음

---
+++ week04/index.php
@@ -0,0 +1,1 @@
+<a href="http://113.198.230.60/week04/makeTable.php?grade=1" target=_blank>1학년 시간표</a><br/>
--- week04/makeTable.php
+++ week04/makeTable.php
@@ -11,9 +11,43 @@
     $str = "";
     $str .= "<table width=" . (180*6) . "px>";
 
-
     // 코드 추가!!
+     $str.= "<colgroup>";
+    for($i=0; $i<6; $i++)$str.= "<col width=160>";
+    $str.= "</colgroup>";
 
+    $str.="<tr>";
+    $str.="<th>교시</th>";
+    // $str.="<th>월</th><th>화</th><th>수</th><th>목</th><th>금</th>";
+    for($j=0; $j<5; $j++){
+      $str.= "<th>".$day[$j]."</th>";     
+    }
+    $str.="</tr>";
+    $spanCount[] = array(0,0,0,0,0,0);
+    for($t=1;$t<10;$t++){ //$t는 시간
+      $str.="<tr align=center>";
+      $str.="<td>{$t}교시<br>".$time[$t]."</td>";
+      for($d=1; $d<6; $d++){ //$t시간의 $d 요일
+        if($spanCount[$d]>0){
+          $spanCount[$d]--;
+          continue;
+        }
+        if($data[$d][$t]==""){
+          $str .= "<td>배열이 비었음{$d}요일{$t}시간</td>";
+          continue;
+        }
+        $exist = false;
+        foreach($data[$d][$t] as $inner){
+          if($inner[0]!=$grade) continue;
+          $exist = true;
+          $str .= "<td rowspan =" .$inner[3]. ">";
+          $str .= "<td>배열" .$inner[1]. "<br>" .$inner[2].$inner[4]."</td>";
+          $spanCount[$d] = $inner[3]-1;
+        }
+        if(!$exist) $str .= "<td>이미 {$d}요일{$t}시간</td>";
+      }
+      $str.="</tr>";
+    }
     
     $str .= "</table>";
     return $str;
