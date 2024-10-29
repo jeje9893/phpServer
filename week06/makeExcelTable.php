8357efbcba442e66f7f23c790179e9cf925a74b6
jk <jeje9893@gmail.com>
2024-10-29 09:30:46 +0900

    수정

--- week06/makeExcelTable.php
+++ week06/makeExcelTable.php
@@ -12,9 +12,10 @@ while ($rows->item($i)) {
     $row = $rows->item($i);
     $cells = $row->getElementsByTagName("Cell");
     for ($j = 0; $j < 6; $j++) {
-        $cell = $cells->$item($j);
+        $cell = $cells->item($j);
         echo $cell->nodeValue . " ";
     }
     echo "<br>";
     $i++;
 }
+?>
